91492b1521b0cd4185819610607ef762bfcf9126
ayaam <ayaam.mit@gmail.com>
2024-10-06 17:52:26 +0530

    we are able to mark x and o

--- src/App.js
+++ src/App.js
@@ -1,39 +1,60 @@
 import React, { useState } from "react";
 
 const Board = () => {
-  const [markX, setMarkX] = useState("X");
+  const [XisNext, setXisNext] = useState(true); //first turn is always of X
+  const[squares, setSquares] = useState(Array(9).fill(null));
+  /**
+   * This will declare the state as an array : ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'];
+   * We will assign each of these states to the Square component and each state will record an individual value
+   */
 
-  function onClickHandler() {
-    if(markX === 'X') setMarkX('O');
-    else setMarkX('X');
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    if(XisNext){
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    setSquares(nextSquares);
+    setXisNext(!XisNext);
   }
+  
+  //   setValue('X');
+  //   console.log('Clicked!');
+  // }
+
+  // function onClickHandler() {
+  //   if(markX === 'X') setMarkX('O');
+  //   else setMarkX('X');
+  // }
 
   return (
     <>
       <div className="board-row" >
-        <MyBox value = {markX} onClickProp={onClickHandler} />
-        <MyBox value = {markX} onClickProp={onClickHandler} />
-        <MyBox value = {markX} onClickProp={onClickHandler} />
+        <Square value = {squares[0]} onClickProp = {() => handleClick(0)} />
+        <Square value = {squares[1]} onClickProp = {() => handleClick(1)} />
+        <Square value = {squares[2]} onClickProp = {() => handleClick(2)} />
       </div>
 
       <div className="board-row">
-        <MyBox value = {markX} onClickProp={onClickHandler} />
-        <MyBox value = {markX} onClickProp={onClickHandler} />
-        <MyBox value = {markX} onClickProp={onClickHandler} />
+        <Square value = {squares[3]} onClickProp = {() => handleClick(3)} />
+        <Square value = {squares[4]} onClickProp = {() => handleClick(4)} />
+        <Square value = {squares[5]} onClickProp = {() => handleClick(5)} />
       </div>
 
       <div className="board-row">
-        <MyBox value = {markX} onClickProp={onClickHandler} />
-        <MyBox value = {markX} onClickProp={onClickHandler} />
-        <MyBox value = {markX} onClickProp={onClickHandler} />
+        <Square value = {squares[6]} onClickProp = {() => handleClick(6)} />
+        <Square value = {squares[7]} onClickProp = {() => handleClick(7)} />  
+        <Square value = {squares[8]} onClickProp = {() => handleClick(8)} />
       </div>
     </>
   );
 };
 
-const MyBox = ({value, onClickProp }) => {
+const Square = ({value, onClickProp}) => {
+  
   return (
-    <button className="square" onClick={onClickProp}>
+    <button className="square" onClick = {onClickProp} >
       {value}
     </button>
   );
