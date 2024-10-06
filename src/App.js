0852df9f1958ecbe6bfb4d93fb29a73753a670a8
ayaam <ayaam.mit@gmail.com>
2024-10-06 16:53:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Board = () => {
+  const [markX, setMarkX] = useState("X");
+
+  function onClickHandler() {
+    if(markX === 'X') setMarkX('O');
+    else setMarkX('X');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="board-row" >
+        <MyBox value = {markX} onClickProp={onClickHandler} />
+        <MyBox value = {markX} onClickProp={onClickHandler} />
+        <MyBox value = {markX} onClickProp={onClickHandler} />
+      </div>
 
-export default App;
+      <div className="board-row">
+        <MyBox value = {markX} onClickProp={onClickHandler} />
+        <MyBox value = {markX} onClickProp={onClickHandler} />
+        <MyBox value = {markX} onClickProp={onClickHandler} />
+      </div>
+
+      <div className="board-row">
+        <MyBox value = {markX} onClickProp={onClickHandler} />
+        <MyBox value = {markX} onClickProp={onClickHandler} />
+        <MyBox value = {markX} onClickProp={onClickHandler} />
+      </div>
+    </>
+  );
+};
+
+const MyBox = ({value, onClickProp }) => {
+  return (
+    <button className="square" onClick={onClickProp}>
+      {value}
+    </button>
+  );
+};
+
+export default Board;
